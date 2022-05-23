55deab69e328b98376879a8f30bde9b6927d4d5b
Gabby <65522279+BobbyTie@users.noreply.github.com>
2022-05-23 19:26:04 +0000

    finished hw5

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     maximum_age_appropriate = models.IntegerField(default=-1, blank=False)
 
     def __str__(self):
-        return f"Product {self.name}, price {self.price:.02f}"
+        return f"{self.name}, price {self.price:.02f}"
 
     def age_range(self):
         if self.maximum_age_appropriate == -1:
@@ -17,3 +17,22 @@ class Product(models.Model):
             return f"Age {self.minimum_age_appropriate}"
         else:
             return f"Ages {self.minimum_age_appropriate} to {self.maximum_age_appropriate}"
+
+    def random_image(self):
+      products = self.productimage_set.all()
+      if not products.exists():
+        return
+      else:
+        products = products.order_by('?')
+        return products[0]
+
+class ProductImage(models.Model):
+  image = models.ImageField(upload_to='product_images/', blank=False)
+  caption = models.CharField(max_length=255, blank=True)
+  product = models.ForeignKey(Product, on_delete = models.CASCADE, blank=False)
+
+  def __str__(self):
+    if (self.caption == None):
+      return  self.product
+    else:
+      return f"{self.product}: {self.caption}"
--- products/views.py
+++ products/views.py
@@ -7,8 +7,21 @@ def index(request):
     products = Product.objects.all().order_by('name')
     form = ProductFilterForm(request.GET)
     name_search = request.GET.get('name_search')
+    sort = request.GET.get('sort')
+    min_price = form['min_price'].value()
+    max_price = form['max_price'].value()
     if name_search:
         products = products.filter(name__icontains=name_search)
+    if sort:
+        if sort == 'ages':
+          products = products.order_by('minimum_age_appropriate')
+        else:
+          products = products.order_by(sort)
+    if min_price:
+      products = products.filter(price__gte=min_price)
+    if max_price:
+      products = products.filter(price__lte=max_price)
+      
 
     context = {'products': products, 'form': form}
     return render(request, 'products/index.html', context)
